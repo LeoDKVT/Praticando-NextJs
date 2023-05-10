4218eea0331b9bc058274dbadba0a8e25f4b1208
José Leonardo <leodkvt@gmail.com>
2023-05-10 07:34:01 -0300

    teste

---
+++ my-app/src/app/page.js
@@ -0,0 +1,13 @@
+
+
+export default function Home() {
+  return (
+    <>
+    <div class="text-5xl font-extrabold ... bg-black	">
+  <span class="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
+    Hello world
+  </span>
+</div>
+    </>
+  );
+}
