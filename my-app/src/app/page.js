472ab99893af11a1ba8af1240f982883090b546c
José Leonardo <leodkvt@gmail.com>
2023-05-12 19:15:15 -0300

    pratica do dia de hoje.

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,14 +1,17 @@
 import Navbar from "@/components/Navbar";
+import Layout from "@/components/layout";
 
 export default function Home() {
   return (
     <>
-    <Navbar />
-      <div className="text-5xl font-extrabold ... bg-black	">
-        <span className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
-          Hello world
-        </span>
-      </div>
+      <Layout>
+        <Navbar />
+        <div className="text-5xl font-extrabold ... bg-black	">
+          <span className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
+            Hello world
+          </span>
+        </div>
+      </Layout>
     </>
   );
 }
